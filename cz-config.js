88ed86ae0fae238d272dcb37cf1c60daca921e6e
Jae Min Ko <jvc123456@khu.ac.kr>
2024-01-10 01:24:27 +0900

    [15] 🌞 Setting: add commit count

--- cz-config.js
+++ cz-config.js
@@ -22,8 +22,18 @@ module.exports = {
       name: '📞  파일 삭제하는 작업만 수행한 경우',
     },
   ],
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
   allowCustomScopes: false,
-  allowBreakingChanges: ['feat', 'fix'],
-  skipQuestions: ['body'],
+  skipQuestions: ["scope", "customScope", "footer"],
   subjectLimit: 100,
 };
