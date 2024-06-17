b38149821e7f4f6dd8c8bde5dc810f49eaa3826e
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-17 10:39:55 +0900

    [25] 🌞 Setting: 모든 md file에 lastmod 자동 생성

--- add-mod-date.sh
+++ add-mod-date.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
+# #!/bin/bash
 
 # 현재 시간을 YYYY-MM-DDTHH:MM:SS+09:00 형식으로 얻기
 current_time=$(date +"%Y-%m-%dT%H:%M:%S+09:00")
 
 # Git stage에 있는 md 파일들 찾기
-staged_files=$(git diff --name-only --cached | grep '[.md\"?]$')
+staged_files=$(git diff --name-only --cached | grep '.md$')
 
 # 각 md 파일에 대해 lastmod 문자열이 포함된 줄을 현재 시간으로 업데이트
 for file in $staged_files; do
-    if grep -q 'lastmod' "$file"; then
-        # lastmod 문자열이 포함된 줄을 현재 시간으로 업데이트
-        sed -i '' -e "s/lastmod: .*/lastmod: $current_time/" "$file"
-    else
-        if grep -q 'date' "$file"; then
-            # date 문자열이 포함된 줄 바로 아래에 lastmod 추가
-            sed -i '' -e "/date:/a\\
+  if grep -q 'lastmod' "$file"; then
+    # lastmod 문자열이 포함된 줄을 현재 시간으로 업데이트
+    sed -i '' -e "s/lastmod: .*/lastmod: $current_time/" "$file"
+    sed -i '' -e "s/lastmod = .*/lastmod = $current_time/" "$file"
+  else
+    # date 문자열이 포함된 줄 바로 아래에 lastmod 추가
+    if grep -q 'date =' "$file"; then
+      # date 문자열이 포함된 줄 바로 아래에 lastmod 추가
+      sed -i '' -e "/date = /a\\
+lastmod = $current_time
+" "$file"
+    elif grep -q 'date:' "$file"; then
+      # date 문자열이 포함된 줄 바로 아래에 lastmod 추가
+      sed -i '' -e "/date: /a\\
 lastmod: $current_time
 " "$file"
-        fi
     fi
-    
-    # 업데이트된 파일을 다시 stage에 추가
-    git add "$file"
+  fi
 done
